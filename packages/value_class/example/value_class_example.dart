320b6af8ac6fb185d952ae8e44f2ff11756c5f56
blendthink <admin@blendthink.dev>
2022-07-25 21:35:32 +0900

    create value_class package

---
+++ packages/value_class/example/value_class_example.dart
@@ -0,0 +1,6 @@
+import 'package:value_class/value_class.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/value_class/lib/value_class.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library value_class;
+
+export 'src/value_class_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
