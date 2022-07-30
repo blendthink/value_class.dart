fbc6caba3cf3639a4d2c8dd540b797eaa2afdc6c
blendthink <admin@blendthink.dev>
2022-07-31 01:29:33 +0900

    Revert "add `set -e` to scripts"
    This reverts commit 0f0c808f1d229bb3104b147648156e09a582b7c1.

--- .github/scripts/copy-credentials.sh
+++ .github/scripts/copy-credentials.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 mkdir -p "$PUB_CACHE"
 cat <<EOF > "$PUB_CACHE"/credentials.json
 $PUB_CREDENTIALS
--- .github/scripts/push-tag.sh
+++ .github/scripts/push-tag.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 tag="$PACKAGE-v$VERSION"
 git tag "$tag"
 git push origin "$tag"
