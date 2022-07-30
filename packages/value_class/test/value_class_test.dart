4a014085621dd89449b6c109d1680cc04a5306bf
blendthink <admin@blendthink.dev>
2022-07-30 15:46:58 +0900

    create test

--- packages/value_class/test/value_class_test.dart
+++ packages/value_class/test/value_class_test.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'dart:io';
+
+import 'package:dartx/dartx_io.dart';
+import 'package:source_gen_test/source_gen_test.dart';
+import 'package:value_annotation/value_annotation.dart';
+import 'package:value_class/src/value_class_generator.dart';
+
+Future<void> main() async {
+  initializeBuildLogTracking();
+  for (final file in Directory('test/successes').listSync(recursive: true)) {
+    if (file is! File || !file.path.endsWith('.dart')) {
+      continue;
+    }
+    final reader = await initializeLibraryReaderForDirectory(
+      file.dirName,
+      file.name,
+    );
+    testAnnotatedElements<ValueClass>(
+      reader,
+      const ValueClassGenerator(),
+    );
+  }
+}
