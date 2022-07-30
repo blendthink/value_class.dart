fa0330482c95cc141d0b0775a3747436011f37f6
blendthink <admin@blendthink.dev>
2022-07-31 01:50:49 +0900

    Revert "add log"
    This reverts commit ea5c03bce1eb586be8d486b28dd1c7feb6b29091.

--- .github/scripts/pubspec-version.sh
+++ .github/scripts/pubspec-version.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-echo 'fetch'
 git fetch origin "$GITHUB_BASE_REF" --depth=1
-
-echo 'diff'
 diff=$(git diff origin/"$GITHUB_BASE_REF"..HEAD -- packages/"$PACKAGE"/pubspec.yaml)
-
-echo 'version'
 version=$(awk '/\+version: /{print $2}' <<<"$diff")
-
-echo 'set-output'
 echo "::set-output name=version::$version"
 
 if [ -z "$version" ]; then
