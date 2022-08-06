57988b61a17df5c727ec9781e66d3c8909a6a2c8
blendthink <admin@blendthink.dev>
2022-08-07 03:20:18 +0900

    export assert package

--- packages/value_annotation/lib/value_annotation.dart
+++ packages/value_annotation/lib/value_annotation.dart
@@ -3,6 +3,7 @@
 library value_annotation;
 
 export 'package:value_annotation/src/value_class.dart';
+export 'package:value_annotation/src/assert.dart';
 
 export 'package:collection/collection.dart' show DeepCollectionEquality;
 export 'package:meta/meta.dart' show immutable;
